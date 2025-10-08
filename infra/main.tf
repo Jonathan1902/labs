c5743f057b805a28e62ecc845a0793bce168db40
Jonathan Costa <jonathancosta888@gmail.com>
2025-10-08 00:24:59 -0300

    feature: include ansible configuration

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,13 @@
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name      = "my-keypair"
 
   tags = {
     Name = "jonnies"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ../ansible/inventory/hosts.ini"
   }
 }
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,2 +1,2 @@
 instance_type="t2.micro"
-ami="ami-0a0d9cf81c479446a"
+ami="ami-052064a798f08f0d3"
